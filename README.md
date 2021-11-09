33e669749e7ec328743ef9f19bb596b497141787
Hussain Nawaz (MBP) <hussain.nwz000@gmail.com>
2021-11-09 12:04:19 +1100

    sanitise args

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # pagerank-comp2521ass2
+## Background
+
+
+
+## Task 1
+
+- [ ] Read tasks
+- [ ] Implement Data Structures
+- [ ] Implement Algorithmns
--- pagerank.c
+++ pagerank.c
@@ -11,7 +11,46 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char *argv[]) {
+void sanitiseArgs(int argc, char *argv[], double *damping, double *diffPR, int *max_iterations);
+void exitBadInput();
 
+
+
+int main(int argc, char *argv[])
+{
+    // Sanitise input from command line
+    double damping;
+    double diffPR;
+    int max_iterations;
+    sanitiseArgs(argc, argv, &damping, &diffPR, &max_iterations);
+    printf("Got: %lf, %lf, %d\n", damping, diffPR, max_iterations);
 }
 
+void sanitiseArgs(int argc, char *argv[], double *damping, double *diffPR, int *max_iterations)
+{
+    assert(argc >= 3);
+    char *endptr;
+    *damping = strtod(argv[1], &endptr);
+    if (endptr == NULL || endptr == argv[1])
+        exitBadInput();
+    if (*damping < 0.0 || *damping > 1.0) 
+        exitBadInput();
+    
+    *diffPR = strtod(argv[2], &endptr);
+    if (endptr == NULL || endptr == argv[2])
+        exitBadInput();
+    if (*diffPR < 1.0)
+        exitBadInput();
+
+    *max_iterations = (int)strtol(argv[3], &endptr, 10);
+    if (endptr == NULL || endptr == argv[3])
+        exitBadInput();
+    if (*max_iterations < 0)
+        exitBadInput();
+}
+
+void exitBadInput()
+{
+    fprintf(stderr, "Invalid Input\n");
+    exit(EXIT_FAILURE);
+}
